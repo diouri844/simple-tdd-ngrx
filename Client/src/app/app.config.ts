a3d09ed92a0e66914a9085c0a54823ac30ece387
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-12-20 11:08:26 +0100

    Update app config

--- Client/src/app/app.config.ts
+++ Client/src/app/app.config.ts
@@ -1,16 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
-import { taskReducer } from './states/tasks/task.reducer';
+import { StoreModule } from '@ngrx/store';
+import { appReducer } from './states/store/app.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { TaskEffects } from './states/tasks/task.effect';
+import { UserEffects } from './states/users/user.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideStore({
-      tasks:taskReducer
-    })
+    // add store : 
+    importProvidersFrom(
+      StoreModule.forRoot( appReducer ),
+      //EffectsModule.forRoot([TaskEffects, UserEffects])
+    )
   ]
 };
