a5d2a8fc340b5b17c94037d6c96732ff8f14e595
Chopen <salahiddine.diouri@usmba.ac.ma>
2024-12-20 11:10:02 +0100

    update user reducer to include loading and error states

--- Client/src/app/states/users/user.reducer.ts
+++ Client/src/app/states/users/user.reducer.ts
@@ -1,13 +1,13 @@
 import { createReducer, on } from '@ngrx/store';
 import { User } from './user.model';
 import { addUser, deleteUser, updateUser, loadUsersSuccess } from './user.action';
+import { UserState } from '../store/app.state';
 
-export interface UserState {
-  users: User[];
-}
 
 const initialState: UserState = {
-  users: []
+  users: [],
+  loading: false,
+  error: null
 };
 
 export const userReducer = createReducer(
